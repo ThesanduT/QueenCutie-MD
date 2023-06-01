4b84c807b9e288af8121f06f7bd427b417dcb0a3
ThesanduT <94118388+ThesanduT@users.noreply.github.com>
2023-06-01 13:27:15 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,273 +1,175 @@
-//═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                                                                     //
-//                                                             ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ                                                               //
-//                                                                                                                                                     //
-//                                                                    Ｖ：１．０．１                                                                     //
-//                                                                                                                                                     //
-//                                     ░█████╗░██╗░░░░░██╗███████╗███╗░░██╗  ░█████╗░██╗░░░░░███████╗░█████╗░                                          //
-//                                     ██╔══██╗██║░░░░░██║██╔════╝████╗░██║  ██╔══██╗██║░░░░░██╔════╝██╔══██╗                                          //
-//                                     ███████║██║░░░░░██║█████╗░░██╔██╗██║  ███████║██║░░░░░█████╗░░███████║                                          //
-//                                     ██╔══██║██║░░░░░██║██╔══╝░░██║╚████║  ██╔══██║██║░░░░░██╔══╝░░██╔══██║                                          //
-//                                     ██║░░██║███████╗██║███████╗██║░╚███║  ██║░░██║███████╗██║░░░░░██║░░██║                                          //
-//                                     ╚═╝░░╚═╝╚══════╝╚═╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░░╚═╝                                          //
-//                                                                                                                                                     //
-//                                                                                                                                                     //
-//═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
-
-// Obfuscated By AlienAlfa
-// Unecrypted file Willbe relesed Soon
-// Full file will be relised After all BUGS Has been Fixed
-
-// CURRENTLY RUNNING ON BETA VERSION!!
-/**
-   * MIT License
-* 
-   * Copyright (c) 2021 Alien-Alfa.
-* 
-   * Permission is hereby granted, free of charge, to any person obtaining a copy
-   * of this software and associated documentation files (the "Software"), to deal
-   * in the Software without restriction, including without limitation the rights
-   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   * copies of the Software, and to permit persons to whom the Software is
-   * furnished to do so, subject to the following conditions:
-* 
-   * The above copyright notice and this permission notice shall be included in all
-   * copies or substantial portions of the Software.
-* 
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   * SOFTWARE.
-*/
-
-
-
-
 const fs = require('fs')
 const chalk = require('chalk')
 
 
-var ownernum = ['0','447405935355']
-//-------------
-
-
-// Website Api
 global.APIs = {
 	zenz: 'https://zenzapis.xyz',
-	devil: 'https://api-toxic-devil.herokuapp.com/api',
-} 
- 
-global.APIKeys = {
-	'https://zenzapis.xyz': '' }
-    
-
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-   //---------------------------------------------------------------------
-
-   global.worktype = 'private'
-
-   //---------------------------------------------------------------------
-   
-   
-   //---------------------------------------------------------------------
-
-   global.sudo = ['447405935355']
-   //-------------
-   global.owner = ['447405935355']
-
-   //---------------------------------------------------------------------
-   
-   global.packname = 'ＡＬＩＥＮ' 
-   //-------------
-   global.author = 'ＡＬＦＡ'
-
-   //---------------------------------------------------------------------
-
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-   //--------------------- MENU URL BUTTON 1 ------------------------
-
-   global.linkbuttid1 = 'ɪɴꜱᴛᴀɢʀᴀᴍ' 
-   //-------------
-   global.butturl1 = 'https://www.instagram.com/alienalfa/'
-   
-   //---------------------------------------------------------------------
-
-
-
-   //--------------------- MENU URL BUTTON 2 ------------------------
-
-   global.linkbuttid2 = 'ʏᴏᴜᴛᴜʙᴇ'
-   //-------------
-   global.butturl2 = 'https://youtube.com/c/alienalfa'
-   
-   //---------------------------------------------------------------------
-
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-   //--------------------- ADPLAY OPTIONS ------------------------
-
-   global.linktext = 'ꜱᴜʙꜱᴄʀɪʙᴇ ᴛᴏ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ'
-   //-------------
-   global.linkurl = 'https://youtube.com/c/alienalfa'
-   
-   //---------------------------------------------------------------------
-
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-   //--------------------- MAIN IMAGE  ------------------------
-
-   global.profileimage = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true'
-      
-   //---------------------------------------------------------------------
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-   //--------------------- URL IMAGE  ------------------------
-
-   global.mainimgurl = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true'
-      
-   //---------------------------------------------------------------------
-
-
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-   //--------------------- MAIN MENU BUTTON ------------------------
-   
-   global.mlinktxt1 = 'ʜᴏᴡ ᴛᴏ ᴅᴇᴘʟᴏʏ ʙᴏᴛ'
-   //-------------
-   global.mlinkurl1 = 'https://youtube.com/c/alienalfa'
-   //------------- 
-   
- //------ URL BUTTON 2 -----
-   global.mlinktxt2 = null
-   //-------------
-   global.mlinkurl2 = null
-   //-------------
- 
- //------ REP BUTTON 1 -----
-   global.mbuttxt1 = 'ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅꜱ'
-   //-------------
-   global.mbutid1 = 'command'
-   //-------------
-   
- //------ REP BUTTON 2 -----
-   global.mbuttxt2 = null 
-   //-------------
-   global.mbutid2 = null
-   //-------------
-   
- //------ REP BUTTON 3 -----
-   global.mbuttxt3 = null
-   //-------------
-   global.mbutid3 = null
-   //-------------
-   
-   //---------------------------------------------------------
-   
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-//============ MAIN ============//
-
-//-------------
-global.premium = [`${ownernum}`]
-//-------------
-global.sessionName = 'session.alfa'
-//-------------
-global.prefa = ['','!','.','🐦','🐤','🗿']
-//-------------
-global.sp = '𒆜'
-//-------------
-global.devname = 'ＡＬＩＥＮ ＡＬＦＡ'
-//-------------
-global.devNum = ['447405935355']
-//-------------
-global.greetings = 'Hey  👋🏻'
-//-------------
-global.listicon = '✩' 
-//-------------
-global.wm = 'ＡＬＩＥＮ ＡＬＦＡ' 
-//-------------
-global.botname = 'ＡＬＩＥＮ ＡＬＦＡ'
-//-------------
-global.icon = '✩' 
-//-------------
-global.mail = 'AlienAlfa.YT@gmail.com'
-//-------------
-global.youtube = 'https://youtube.com/c/alienalfa'
-//-------------
-global.github = 'https://github.com/Alien-alfa/' 
-//-------------
-global.insta = 'https://www.instagram.com/alienalfa/' 
-//-------------
-global.linkname = '𝘼𝙇𝙄𝙀𝙉 𝘼𝙇𝙁𝘼'
-//-------------
-global.mainfooter = '𝘼𝙇𝙄𝙀𝙉 𝘼𝙇𝙁𝘼' 
-//-------------
-global.herokuapi = '' 
-//-------------
-global.herokuapp = '' 
-
-
-//|⬡════════════════════════════════════════════|❝ Ⓒ𝙰𝙻𝙸𝙴𝙽 𝙰𝙻𝙵𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙾𝚇𝙸𝙲 𝙰𝙻𝙸𝙴𝙽™ ❞|═══════════════════════════════════════════⬡|//	
-
-
-//============ Modz ============//
-
-//-------------
-global.welcomeactive =  true
-//-------------
-global.alivemess = false
-//-------------
-global.aliveimage = false
-//-------------
-global.speedmode =  'active'
-//-------------
-global.ownermess = 'ʜᴇʏ ᴛʜɪꜱ ɪꜱ ᴍʏ ᴏᴡɴᴇʀ, ᴘʟᴇᴀꜱᴇ ᴛᴀʟᴋ ɪɴ ᴇɴɢʟɪꜱʜ'
-//-------------
-global.autoread = false 
-//-------------
-global.anticall = false 
-//-------------
-
-
-
-
-
-global.thumb = fs.readFileSync('./lib/lowdb/adapters/alfa.jpg')
-
-global.mess = {
-    success: 'ᴅᴏɴᴇ',
-    errr: 'err',
-    admin: 'ᴏɴʟʏ ᴀᴅᴍɪɴ',
-    botAdmin: 'ɪ ᴡɪʟʟ ᴅᴏ ɪᴛ, ᴍᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ!',
-    owner: 'ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅ!',
-    group: 'ᴛʜɪꜱ ɪꜱ ɴᴏᴛ ᴀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ!',
-    private: 'ᴛʜɪꜱ ɪꜱ ᴀ ɢʀᴏᴜᴘ, ᴜꜱᴇ ɪᴛ ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ!',
-    bot: '',
-    wait: 'ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ...',
-    endLimit: 'ʏᴏᴜʀ ᴅᴀɪʟʏ ʟɪᴍɪᴛ ʜᴀꜱ ᴇxᴘɪʀᴇᴅ, ᴛʜᴇ ʟɪᴍɪᴛ ᴡɪʟʟ ʙᴇ ʀᴇꜱᴇᴛ ᴇᴠᴇʀʏ 12 ʜᴏᴜʀꜱ',
-    notext: 'ᴇɴᴛᴇʀ ꜱᴇᴀʀᴄʜ ʟɪɴᴋꜱ!',
 }
+
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+const _0x138100=_0x2ef9;(function(_0x11971a,_0x460754){const _0x419220=_0x2ef9,_0x594e91=_0x11971a();while(!![]){try{const _0x46f0c8=-parseInt(_0x419220(0x1b4))/0x1*(parseInt(_0x419220(0x1b1))/0x2)+parseInt(_0x419220(0x1a7))/0x3+parseInt(_0x419220(0x1ae))/0x4*(parseInt(_0x419220(0x1ba))/0x5)+parseInt(_0x419220(0x1a6))/0x6*(parseInt(_0x419220(0x1b9))/0x7)+-parseInt(_0x419220(0x1aa))/0x8*(-parseInt(_0x419220(0x1bb))/0x9)+parseInt(_0x419220(0x1ad))/0xa+-parseInt(_0x419220(0x1a4))/0xb*(parseInt(_0x419220(0x1b7))/0xc);if(_0x46f0c8===_0x460754)break;else _0x594e91['push'](_0x594e91['shift']());}catch(_0x32eb15){_0x594e91['push'](_0x594e91['shift']());}}}(_0x286d,0x1aaae));function _0x286d(){const _0x2089ff=['SERVER','108iDsEXg','sessionidzz','581OZrLVM','142895Vtvtsc','18rHyPnx','BETA\x202.1.0','env','315513omPOfr','SessName','13182FqOGtC','153570ZxdVFx','devNumz','SESSION_ID','168160yXSXYQ','HEROKU_APP_NAME','Local\x20Host','1303410lFUlhr','12nMMtpB','gversion','herokuapp','248564sBtWID','isHeroku','Local','1vLEpWL','bodong'];_0x286d=function(){return _0x2089ff;};return _0x286d();}const {smsg,formatp,tanggal,formatDate,getTime,isUrl,sleep,clockString,runtime,fetchJson,getBuffer,jsonformat,format,parseMention,getRandom}=require('./lib/myfunc');function _0x2ef9(_0x1c4eaf,_0x375c67){const _0x286dd6=_0x286d();return _0x2ef9=function(_0x2ef9c6,_0x16d127){_0x2ef9c6=_0x2ef9c6-0x1a2;let _0x5e257f=_0x286dd6[_0x2ef9c6];return _0x5e257f;},_0x2ef9(_0x1c4eaf,_0x375c67);}global[_0x138100(0x1b2)]=process[_0x138100(0x1a3)][_0x138100(0x1b6)]===undefined?_0x138100(0x1b3):process[_0x138100(0x1a3)][_0x138100(0x1b6)];if(isHeroku===_0x138100(0x1b3))yisHeroku=![];else yisHeroku=!![];if(yisHeroku===![])global[_0x138100(0x1b5)]='./lib/alienalfa.js';global[_0x138100(0x1b8)]=process['env'][_0x138100(0x1a9)]===undefined?![]:process[_0x138100(0x1a3)][_0x138100(0x1a9)],global[_0x138100(0x1af)]=_0x138100(0x1a2),global[_0x138100(0x1b0)]=process[_0x138100(0x1a3)][_0x138100(0x1ab)]===undefined?_0x138100(0x1ac):process[_0x138100(0x1a3)][_0x138100(0x1ab)],global[_0x138100(0x1a5)]='session.alfa.json',global[_0x138100(0x1a8)]=['447405935355','919383400679'];
+
+
+
+
+global.autoTyping = false     //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false  //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false     //auto reading in gc (true to on, false to off)
+global.autoRecord = false     //auto recording (true to on, false to off)
+global.available = true       //auto available (true to on, false to off)
+
+
+
+
+
+global.vidmenuz = 'https://tenor.com/bC4cO.gif' //gif and video menu
+
+
+
+
+
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+
+
+
+
+global.vcardowner = ['447405935355'] //ur owner number
+
+global.ownername = '𝘼𝙇𝙄𝙀𝙉 𝘼𝙇𝙁𝘼' //ur owner name
+
+global.ytname = "YT: AlienAlfa" //ur yt chanel name
+
+global.socialm = "GitHub: Alien-Alfa" //ur github or insta name
+
+global.location = "UK, London, hertfordshire" //ur location
+
+
+
+
+global.owner = ['447405935355']
+
+global.ownertag = '447405935355' //ur tag number
+
+global.botname = "𝒜𝓊𝓇𝑜𝓇𝒶 𝐵𝒪𝒯-𝑀𝒟" //ur bot name
+
+global.linkz = 'https://www.youtube.com/alienalfa' //your theme url which will be displayed on whatsapp
+
+global.websitex = 'https://www.GITHUB.com/alien-alfa' //ur website to be displayed
+
+global.botscript = 'https://github.com/Alien-Alfa/WhatsAppBot-MD-BETA2' //script link
+
+global.reactmoji = "😘" //ur menu react emoji
+
+global.themeemoji = "🐤" //ur theme emoji
+
+global.packname = '🐥 𝘼𝙇𝙄𝙀𝙉 𝘼𝙇𝙁𝘼-𝙈𝘿 \n😻 T\n🥰 E\n🤩\n🥹 A\n🥳 M\n😜 O\n😏\n😘 C\n🤪 A\n😳 R\n🫠 I\n🫡 N\n🥺 O\n' //ur sticker watermark packname
+
+global.author = '\n△   ᴀʟɪᴇɴᴀʟꜰᴀ\n▷   ꜱᴀꜰᴡᴀɴɢᴀɴᴢ\n◁   ᴀᴊᴍᴀʟ\n△   ɴᴇᴇʀᴀᴊ \n▷   xᴇᴏɴ ' //ur sticker watermark author
+
+
+
+
+
+global.thumz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur thumb pic
+
+global.logoz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur logo pic
+
+global.err4rz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur error pic
+
+global.thumbz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur thumb pic
+
+
+
+
+
+global.premium = global.premiumzz || ['447405935355'] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+
+
+
+global.antitags = global. true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '𒆜'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: '```Processing...```',
+    linkm: 'Where is the link?',
+    error: 'Error!!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+
+global.SessName = 'session'
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+let worktypez = process.env.WORKTYPE === undefined ? 'Private' : process.env.WORKTYPE
+//-------------
+let workres = worktypez.toLowerCase()  
+//-------------
+if (workres === 'public') {global.worktype = 'public'} 
+//-------------
+else {global.worktype = 'private'}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+global.gversion = '1.8.0 BETA TEST'
+global.herokuapp = 'Local Host'
+
+
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: 12,
+    monayawal: 1000
 }
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 
 
 let file = require.resolve(__filename)
